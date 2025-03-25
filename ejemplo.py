062885159c7d97e89f8e130bfbe2563fde3408c8
SamuelBustaM <s.bustamante@udea.edu.co>
2025-03-25 17:48:24 -0500

    Ejemplo uno y commit change 🤣

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+" Informatica 1 "
+a=int(input("ingrese un numero "))
+b=int(input(" ingrese otro numero "))
+c=0
+resultado=0
+while c< a:
+    resultado+=b
+    print(resultado)
+    c+=1
+print(f"Resultado final: {resultado}")
+
+
+
